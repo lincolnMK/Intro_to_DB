ac3049778b77cc1aa58407d6367885e420e2bf12
kings mkumbwa <kingsmkumbwa@hotmail.com>
2025-12-14 13:58:21 +0200

    kaya man

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,16 +1,23 @@
 import mysql.connector
 
 # Replace with your connection details
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="rentalmanagement"
-)
 
-mycursor = mydb.cursor()
+try: 
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+    
+    )
+    mycursor = mydb.cursor()
 # Execute SQL statements using the execute() method on the cursor
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+    exit(1)
+
+
 sql = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+
 if mycursor.execute(sql):
     print(" Database 'alx_book_store' created successfully!")
 else:
