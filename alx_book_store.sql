b05b92ea2c4d0c8857ff2d44ecb80f4e6b430579
kings mkumbwa <kingsmkumbwa@hotmail.com>
2025-12-14 13:36:54 +0200

    syntacx

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,34 +1,34 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store
 CREATE TABLE Books (
-    book_id (PRIMARY KEY)
+    book_id INT (PRIMARY KEY)
     title VARCHAR(130)
-    author_id (FOREIGN KEY referencing Authors TABLE)
+    (FOREIGN KEY (author_id) REFERENCES Authors (author_id))
     price DOUBLE
     publication_date DATE
 );
 CREATE TABLE Authors (
-    author_id (PRIMARY KEY)
+    author_id INT (PRIMARY KEY)
     author_name VARCHAR(215)
 );
 CREATE TABLE Customers(
-    customer_id (PRIMARY KEY)
+    customer_id INT (PRIMARY KEY)
     customer_name VARCHAR(215)
     email VARCHAR(215)
     address TEXT
 );
 
 CREATE TABLE Orders(
-    order_id (PRIMARY KEY)
-    customer_id (FOREIGN KEY referencing Customers TABLE)
+    order_id INT (PRIMARY KEY)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
     order_date DATE
 
 );
 
 CREATE TABLE Order_Details(
 
-    orderdetailid (PRIMARY KEY)
-    order_id (FOREIGN KEY referencing Orders TABLE)
-    book_id (FOREIGN KEY referencing Books TABLE)
+    orderdetailid INT (PRIMARY KEY)
+    (FOREIGN KEY (order_id) REFERENCES Orders (order_id))
+    (FOREIGN KEY (book_id) REFERENCES Books (book_id))
     quantity DOUBLE
 );
 
